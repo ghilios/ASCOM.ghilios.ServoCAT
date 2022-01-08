973daff8880bc9c0bc59e9c24cdb294f0cf7f708
George Hilios <ghilios@gmail.com>
2022-01-08 14:33:57 -0500

    Rev version

--- ServoCATDriver/Properties/AssemblyInfo.cs
+++ ServoCATDriver/Properties/AssemblyInfo.cs
@@ -1,4 +1,16 @@
-﻿using System.Reflection;
+﻿#region "copyright"
+
+/*
+    Copyright © 2021 - 2021 George Hilios <ghilios+SERVOCAT@googlemail.com>
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#endregion "copyright"
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("ServoCAT ASCOM Server, by George Hilios")]
@@ -9,8 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © 2021, George Hilios")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
-
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
 [assembly: ComVisibleAttribute(false)]
